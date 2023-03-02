77d13371c686ea8c013116d22c3142087e18573f
pvkhoi901 <quang.khoi.589@gmail.com>
2023-03-02 21:35:48 +0700

    fix image

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -2,7 +2,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
       <a href="/admin" class="brand-link">
-          <span class="brand-text font-weight-light">AdminLTE 3</span>
+          <span class="brand-text font-weight-light">Admin</span>
       </a>
 
       <!-- Sidebar -->
